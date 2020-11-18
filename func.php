7e0c44eac344d3f64498a62011c7103fe790f781
TheParanoik <41195328+TheParanoik@users.noreply.github.com>
2020-11-18 09:38:47 +0100

    Update func.php

--- func.php
+++ func.php
@@ -1,35 +1,51 @@
 <?php
+
 session_start();
+
 $xsrf-error = array();
-function xsrf_request_domain($domain){
+function xsrf_request_domain( $domain ){
+   
    $selfDomain = $_SERVER['SERVER_NAME'];
    $request = parse_url($_SERVER['HTTP_REFERER']);
    $reqDomain = $referer['host'];
-if($selfDomain =! $reqDomain){
- array_push($xsrf-error,"Request domain invalid");
- die();
-}
+   
+      if( $selfDomain =! $reqDomain ){
+       array_push( $xsrf-error, "Request domain invalid" );
+       die();
+      }
+   
   }
+
 function xsrf_request_src($url){
+   
    $expectedURL = $url;
    $reqURL = $_SERVER['HTTP_REFERER'];
- array_push($xsrf-error, "Request referer URL invalid. Expected request source:".$url);
- die();
+   
+   array_push($xsrf-error, "Request referer URL invalid. Expected request source:". $url);
+   
+   die();
+   
   }
+
 function xsrf-token($opt, $type){
 
   }
+
 function xsrf_createsum($addsalt){
    $domain = $_SERVER['SERVER_NAME'];
    $UA = $_SERVER['HTTP_USER_AGENT'];
    $form = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'];
    $token = $domain.$UA.$form;
+   
    if(isset($addsalt)){
    $token = $token.$addstalt;
    }
+   
    $token = md5($token);
    echo "<input type='hidden' name='xsrf-token' value='".$token."'>";
+   
 }
+
 function xsrf_verifysum($addsalt){
    $domain = $_SERVER['SERVER_NAME'];
    $UA = $_SERVER['HTTP_USER_AGENT'];
@@ -41,13 +57,20 @@ function xsrf_verifysum($addsalt){
    $token = md5($token);
    
    if(!isset($_POST['token']) || !isset($_GET['token'])){
+      
     array_push($xsrf-error,"No token created");
+      
     die();
-   }elseif($_POST['token'] =! $token && $_GET['token'] =! $token){
+      
+   }elseif( $_POST['token'] =! $token && $_GET['token'] =! $token ){
+      
     array_push($xsrf-error,"Token invalid");
+      
     die();
+      
    }
   }
+
 function xsrf_error(){
  $_SESSION['xsrferr'] = $xsrf-error;
  header("Location:xsrf-err.php");
